06946acf5a39f9014e3c0e5d79a5cd13cbfc63da
khaledbakeer <bakeer@khaled.de>
2019-08-18 17:11:57 +0200

    create storage helper using Dependency Service

---
+++ XamarinFormsSpecialFolders/Helpers/StorageHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using XamarinFormsSpecialFolders.Interfaces;
+
+namespace XamarinFormsSpecialFolders.Helpers
+{
+    public static class StorageHelper
+    {
+        public static List<string> GetSpecialFolders()
+        {
+            return Xamarin.Forms.DependencyService.Get<IFileHelper>().GetSpecialFolders();
+        }
+    }
+}
