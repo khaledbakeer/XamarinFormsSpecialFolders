e3aae306cb5202731c2400c2048f2515e872102c
khaledbakeer <bakeer@khaled.de>
2019-08-18 18:54:22 +0200

    add list to view special folders

--- XamarinFormsSpecialFolders/MainPage.xaml.cs
+++ XamarinFormsSpecialFolders/MainPage.xaml.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using Xamarin.Forms;
+using XamarinFormsSpecialFolders.ViewModels;
 
 namespace XamarinFormsSpecialFolders
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
@@ -17,7 +11,7 @@ namespace XamarinFormsSpecialFolders
         {
             InitializeComponent();
 
-            var availableFolders = Helpers.StorageHelper.GetSpecialFolders();
+            BindingContext = new FolderViewModel();
         }
     }
 }
