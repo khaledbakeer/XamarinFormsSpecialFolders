fc2797d28e8807ab77ef4a4665fc3f73417f4203
khaledbakeer <bakeer@khaled.de>
2019-08-18 18:53:47 +0200

    create FolderViewModel

---
+++ XamarinFormsSpecialFolders/ViewModels/FolderViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using XamarinFormsSpecialFolders.Models;
+
+namespace XamarinFormsSpecialFolders.ViewModels
+{
+    public class FolderViewModel
+    {
+        #region Fields
+        private List<FolderModel> availableFolders = new List<FolderModel>();
+        #endregion
+
+        #region Properties
+        public List<FolderModel> AvailableFolders { get => availableFolders; set => availableFolders = value; }
+        #endregion
+
+        #region Constructor
+        public FolderViewModel()
+        {
+            List<string> stringFolders = Helpers.StorageHelper.GetSpecialFolders();
+
+            foreach (var folder in stringFolders)
+            {
+                availableFolders.Add(new FolderModel(folder));
+            }
+        }
+        #endregion
+    }
+}
