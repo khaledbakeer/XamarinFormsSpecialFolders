37a00849980f3bce6d82165fa95f0303bf605561
khaledbakeer <bakeer@khaled.de>
2019-08-18 16:55:22 +0200

    create FileHelper Class on Android as draft

---
+++ XamarinFormsSpecialFolders.Android/DependencyServices/FileHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using XamarinFormsSpecialFolders.Droid.DependencyServices;
+using XamarinFormsSpecialFolders.Interfaces;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace XamarinFormsSpecialFolders.Droid.DependencyServices
+{
+    public class FileHelper : IFileHelper
+    {
+        public List<string> GetSpecialFolders()
+        {
+            List<string> folders = new List<string>();
+
+            foreach (var folder in Enum.GetValues(typeof(Environment.SpecialFolder)))
+            {
+                if (!string.IsNullOrEmpty(Environment.GetFolderPath((Environment.SpecialFolder)folder)))
+                {
+                    folders.Add($"{folder}={Environment.GetFolderPath((Environment.SpecialFolder)folder)}");
+                }
+            }
+
+            return folders;
+        }
+    }
+}
