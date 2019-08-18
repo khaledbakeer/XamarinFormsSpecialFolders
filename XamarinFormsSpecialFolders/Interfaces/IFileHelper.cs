9ae281dda8c1df9767313c6a5e6811871a43496d
khaledbakeer <bakeer@khaled.de>
2019-08-18 16:54:44 +0200

    create the interface

---
+++ XamarinFormsSpecialFolders/Interfaces/IFileHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XamarinFormsSpecialFolders.Interfaces
+{
+    public interface IFileHelper
+    {
+        List<string> GetSpecialFolders();
+    }
+}
