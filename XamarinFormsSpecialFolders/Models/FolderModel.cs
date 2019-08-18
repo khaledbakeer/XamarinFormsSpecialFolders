a64d8d92fd840cbfcbc05ecf2b3a43e845a0e01d
khaledbakeer <bakeer@khaled.de>
2019-08-18 18:53:24 +0200

    create FolderModel

---
+++ XamarinFormsSpecialFolders/Models/FolderModel.cs
@@ -0,0 +1,16 @@
+﻿namespace XamarinFormsSpecialFolders.Models
+{
+    public class FolderModel
+    {
+        #region Properties
+        public string FolderName { get; set; }
+        #endregion
+
+        #region Constructor
+        public FolderModel(string folderName)
+        {
+            FolderName = folderName;
+        }
+        #endregion
+    }
+}
