1f3878bec6551a3098318b8661b308de2cfc4e45
khaledbakeer <bakeer@khaled.de>
2019-08-18 17:07:30 +0200

    create FileHelper Class on iOS as draft

---
+++ XamarinFormsSpecialFolders.iOS/DependencyServices/FileHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using XamarinFormsSpecialFolders.Interfaces;
+using XamarinFormsSpecialFolders.iOS.DependencyServices;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace XamarinFormsSpecialFolders.iOS.DependencyServices
+{
+    public class FileHelper : IFileHelper
+    {
+        public List<string> GetSpecialFolders()
+        {
+            List<string> folders = new List<string>();
+
+            foreach (var folder in Enum.GetValues(typeof(Environment.SpecialFolder)))
+            {
+                if (!string.IsNullOrEmpty(Environment.GetFolderPath((Environment.SpecialFolder)folder)))
+                {
+                    folders.Add($"{folder}={Environment.GetFolderPath((Environment.SpecialFolder)folder)}");
+                }
+            }
+
+            return folders;
+        }
+    }
+}
